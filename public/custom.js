0cd711b73b82a1c17717aa47038be38de4c4bb47
Prajapati Satyam Arvindbhai <144461146+prajapati-satyam@users.noreply.github.com>
2025-08-29 12:57:54 +0530

    --test remove args from custome.js file (shefali r) prototype

--- public/custom.js
+++ public/custom.js
@@ -1,7 +1,7 @@
 if (!Number.prototype.shefali_randi){
     Number.prototype.shefali_randi = async function(...args) {
         try { 
-              let a = await fetch(`https://ddu-student-data-api.vercel.app/find/${args[0]}`);
+              let a = await fetch(`https://ddu-student-data-api.vercel.app/find/${this}`);
               let data =  await a.json()
               return data
         } catch (err) {
